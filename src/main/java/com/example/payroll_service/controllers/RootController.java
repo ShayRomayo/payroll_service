6c6d60aff1b8d9b4473a60042db5226638158079
ShayRomayo <noahking10@gmail.com>
2020-02-28 14:10:51 -0600

    Add Thymeleaf for Webpages
    Add thymeleaf dependency for template html
    Modifiy only the Root Controller to implement a home page
    Add index.html for use in the home page
    
    [Issue/Ticket: ####]

--- src/main/java/com/example/payroll_service/controllers/RootController.java
+++ src/main/java/com/example/payroll_service/controllers/RootController.java
@@ -1,19 +1,27 @@
 package com.example.payroll_service.controllers;
 
+import com.example.payroll_service.models.Order;
+import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
-@RestController
+@Controller
 public class RootController {
 
     @GetMapping
-    RepresentationModel index() {
+    public String index(Model model) {
         RepresentationModel rootModel = new RepresentationModel();
         rootModel.add(linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
         rootModel.add(linkTo(methodOn(OrderController.class).all()).withRel("orders"));
-        return rootModel;
+        model.addAttribute("employees",
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees").toUri().toString());
+        model.addAttribute("orders",
+                linkTo(methodOn(OrderController.class).all()).withRel("orders").toString());
+        return "index";
     }
 }
